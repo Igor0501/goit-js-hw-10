30a3ac905ed33bd6b85e0faaefcd0615ddb162d7
Igor0501 <s.igor0501@gmail.com>
2023-05-21 03:28:59 +0300

    update_fetch

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,25 @@
-const BASE_URL = 'https://restcountries.com/v3.1/name';
+export function fetchCountries(name) {
+  const params = new URLSearchParams({
+    fields: 'name,flags,capital,population,languages',
+  });
+  return fetch(`https://restcountries.com/v3.1/name/${name}?${params}`).then(
+    countries => {
+      if (!countries.ok) {
+        throw new Error(countries.status);
+      }
+      return countries.json();
+    }
+  );
+}
 
-const fetchCountries = async name => {
-  const response = await fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`);  
-  if (response.status >= 400 && response.status <= 599) {    
-    return Promise.reject(new Error());    
-  }
-  return await response.json();
-};
+// const BASE_URL = 'https://restcountries.com/v3.1/name';
 
-export { fetchCountries };// іменований експорт раніше оголошеної функції
+// const fetchCountries = async name => {
+//   const response = await fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`);  
+//   if (response.status >= 400 && response.status <= 599) {    
+//     return Promise.reject(new Error());    
+//   }
+//   return await response.json();
+// };
+
+// export { fetchCountries };// іменований експорт раніше оголошеної функції
